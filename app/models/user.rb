f2c946dab268e5db85c8944c155a7a822d815b8c
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-08-24 20:02:52 +0200

    Add devise configuration to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :trackable,
+         :validatable
+
   validates :name,
             presence: true
 
@@ -21,13 +27,22 @@ end
 #
 # Table name: users
 #
-#  id         :uuid             not null, primary key
-#  email      :string           default(""), not null, uniquely indexed
-#  name       :string           default(""), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :uuid             not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null, uniquely indexed
+#  encrypted_password     :string           default(""), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  name                   :string           default(""), not null
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string           uniquely indexed
+#  sign_in_count          :integer          default(0), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email  (email) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
